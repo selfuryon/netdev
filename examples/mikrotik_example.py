50e3f8eaeb2e9fbec90a09bb90757c976244efec
Yakovlev Sergey <selfuryon@gmail.com>
2016-06-22 20:57:34 +0500

    Adding mikrotik example stript

---
+++ examples/mikrotik_example.py
@@ -0,0 +1,32 @@
+import asyncio
+import logging
+
+import yaml
+
+import netdev
+
+config_path = 'config.yaml'
+
+logging.basicConfig(level=logging.WARN, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+async def task(param):
+    routeros = netdev.connect(**param)
+    await routeros.connect()
+    commands = ['/ip address', 'print', '/']
+    for cmd in commands:
+        print(await routeros.send_command(cmd))
+
+
+async def run():
+    config = yaml.load(open(config_path, 'r'))
+    devices = yaml.load(open(config['device_credentials'], 'r'))
+    params = [p for p in devices if p['device_type'] == 'mikrotik_routeros']
+    tasks = []
+    for param in params:
+        tasks.append(task(param))
+    await asyncio.wait(tasks)
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run())
