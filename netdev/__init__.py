92f7b1d4ed7d6bbccf966e0db0fa7889c288a05e
Yakovlev Sergey <selfuryon@gmail.com>
2016-07-18 00:56:19 +0500

    Adding logger in module init

--- netdev/__init__.py
+++ netdev/__init__.py
@@ -1,7 +1,8 @@
 from netdev.dispatcher import connect
 from netdev.dispatcher import platforms
 from netdev.exceptions import DisconnectError
+from netdev.logger import logger
 
 __version__ = '0.3.0'
-__all__ = ('connect', 'platforms', 'DisconnectError')
+__all__ = ('connect', 'platforms', 'DisconnectError', 'logger')
 __author__ = 'Yakovlev Sergey'
