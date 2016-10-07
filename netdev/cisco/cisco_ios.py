53579873e74a524dbedf3cd90809fdb6681cfc39
Yakovlev Sergey <selfuryon@gmail.com>
2016-10-07 22:21:59 +0500

    Changing CiscoIos docstrings

--- netdev/cisco/cisco_ios.py
+++ netdev/cisco/cisco_ios.py
@@ -2,7 +2,5 @@ from ..cisco_like import CiscoLikeDevice
 
 
 class CiscoIos(CiscoLikeDevice):
-    """
-    Subclass specific fo Cisco IOS/IOS XE
-    """
+    """Class for working with Cisco IOS/IOS XE"""
     pass
