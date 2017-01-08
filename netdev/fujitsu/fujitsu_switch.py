41974e73e8e07df6871bfac0586b408fa20f5c18
Yakovlev Sergey <s.yakovlev@hostco.ru>
2017-01-09 02:44:15 +0500

    Refactoring fujitsu switch class

--- netdev/fujitsu/fujitsu_switch.py
+++ netdev/fujitsu/fujitsu_switch.py
@@ -6,6 +6,11 @@ from ..logger import logger
 
 class FujitsuSwitch(IOSLikeDevice):
     """Class for working with Fujitsu Blade switch"""
+
+    _pattern = r"\({}.*?\) (\(.*?\))?[{}]"
+    _disable_paging_command = 'no pager'
+    _config_enter = 'conf'
+
     async def _set_base_prompt(self):
         """
         Setting two important vars
@@ -18,11 +23,9 @@ class FujitsuSwitch(IOSLikeDevice):
         prompt = await self._find_prompt()
         # Strip off trailing terminator
         self._base_prompt = prompt[1:-3]
-        delimeter1 = self._get_default_command('delimeter1')
-        delimeter2 = self._get_default_command('delimeter2')
-        pattern = self._get_default_command('pattern')
-        self._base_pattern = pattern.format(re.escape(self._base_prompt[:12]), re.escape(delimeter1),
-                                            re.escape(delimeter2))
+        delimiters = r"|".join(type(self)._delimiter_list)
+        pattern = type(self)._pattern
+        self._base_pattern = pattern.format(self._base_prompt[:12], delimiters)
         logger.debug("Host {}: Base Prompt: {}".format(self._host, self._base_prompt))
         logger.debug("Host {}: Base Pattern: {}".format(self._host, self._base_pattern))
         return self._base_prompt
@@ -34,25 +37,3 @@ class FujitsuSwitch(IOSLikeDevice):
         """
         newline = re.compile(r'(\r\r\n|\r\n|\n\r)')
         return newline.sub('\n', a_string).replace('\n\n', '\n')
-
-    def _get_default_command(self, command):
-        """
-        Returning default commands for device
-
-        :param command: command for returning
-        :return: real command for this network device
-        """
-        # @formatter:off
-        command_mapper = {
-            'delimeter1': '>',
-            'delimeter2': '#',
-            'pattern': r"\({}.*?\) (\(.*?\))?[{}|{}]",
-            'disable_paging': 'no pager',
-            'priv_enter': 'enable',
-            'priv_exit': 'disable',
-            'config_enter': 'conf',
-            'config_exit': 'end',
-            'config_check': ')#',
-        }
-        # @formatter:on
-        return command_mapper[command]
