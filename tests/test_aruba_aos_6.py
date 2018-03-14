90dad1ab5aace8ce6f8c1f5a011b00d7004ea06f
Yakovlev Sergey <selfuryon@gmail.com>
2018-03-14 23:14:48 +0500

    Adding unit test for aruba aos 6.x

---
+++ tests/test_aruba_aos_6.py
@@ -0,0 +1,68 @@
+import asyncio
+import logging
+import unittest
+
+import yaml
+
+import netdev
+
+logging.basicConfig(filename='unittest.log', level=logging.DEBUG)
+config_path = 'config.yaml'
+
+
+class TestAOS6(unittest.TestCase):
+    @staticmethod
+    def load_credits():
+        with open(config_path, 'r') as conf:
+            config = yaml.load(conf)
+            with open(config['device_list'], 'r') as devs:
+                devices = yaml.load(devs)
+                params = [p for p in devices if p['device_type'] == 'aruba_aos_6']
+                return params
+
+    def setUp(self):
+        self.loop = asyncio.new_event_loop()
+        self.loop.set_debug(False)
+        asyncio.set_event_loop(self.loop)
+        self.devices = self.load_credits()
+        self.assertFalse(len(self.devices) == 0)
+
+    def test_show_run_hostname(self):
+        async def task():
+            for dev in self.devices:
+                async with netdev.create(**dev) as aos:
+                    out = await aos.send_command('show run | i hostname')
+                    self.assertIn("hostname", out)
+
+        self.loop.run_until_complete(task())
+
+    def test_show_several_commands(self):
+        async def task():
+            for dev in self.devices:
+                async with netdev.create(**dev) as aos:
+                    commands = ["dir", "show ver", "show run", "show ssh"]
+                    for cmd in commands:
+                        out = await aos.send_command(cmd, strip_command=False)
+                        self.assertIn(cmd, out)
+
+        self.loop.run_until_complete(task())
+
+    def test_config_set(self):
+        async def task():
+            for dev in self.devices:
+                async with netdev.create(**dev) as aos:
+                    commands = ["interface console", "exit"]
+                    out = await aos.send_config_set(commands)
+                    self.assertIn("line con 0", out)
+                    self.assertIn("exit", out)
+
+        self.loop.run_until_complete(task())
+
+    def test_base_prompt(self):
+        async def task():
+            for dev in self.devices:
+                async with netdev.create(**dev) as ios:
+                    out = await ios.send_command('sh run | i hostname')
+                    self.assertIn(ios.base_prompt, out)
+
+        self.loop.run_until_complete(task())
