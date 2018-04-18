5f98620b98b19a51a130616439cb054bf72795ef
Yakovlev Sergey <selfuryon@gmail.com>
2018-04-18 23:58:47 +0500

    Adding timeout for connecting and reading operation

--- netdev/vendors/exceptions.py
+++ netdev/vendors/exceptions.py
@@ -8,45 +8,10 @@ class DisconnectError(Exception):
         super().__init__(self.msg)
 
 
-class PasswordChangeRequired(Exception):
-    """AsyncSSH PasswordChangeRequired Error with ip address"""
-    def __init__(self, ip_address, prompt):
-        self.prompt = prompt
-        self.msg = "Host {} Password change required:{}".format(ip_address, prompt)
-        super().__init__(self.msg)
+class TimeoutError(Exception):
+    """concurrent.futures._base.TimeoutError with ip address"""
 
-
-class BreakReceived(Exception):
-    """AsyncSSH BreakReceived Error with ip address"""
-    def __init__(self, ip_address, msec):
-        self.msec = msec
-        self.msg = "Host {} Break for {} msec".format(ip_address, msec)
-        super().__init__(self.msg)
-
-
-class SignalReceived(Exception):
-    """AsyncSSH SignalReceived Error with ip address"""
-    def __init__(self, ip_address, signal):
-        self.signal = signal
-        self.msg = "Host {} signal received:{}".format(ip_address, signal)
-        super().__init__(self.msg)
-
-
-class TerminalSizeChanged(Exception):
-    """AsyncSSH TerminalSizeChanged Error with ip address"""
-    def __init__(self, ip_address, width, height, pixwidth, pixheight):
-        self.width = width
-        self.height = height
-        self.pixwidth = pixwidth
-        self.pixheight = pixheight
-        self.msg = "Host {} Terminal size changes:({},{},{},{})".format(ip_address, width, height, pixwidth, pixheight)
-        super().__init__(self.msg)
-
-
-class ChannelOpenError(Exception):
-    """AsyncSSH ChannelOpenError Error with ip address"""
-    def __init__(self, ip_address, code, reason):
-        self.code = code
-        self.reason = reason
-        self.msg = "Host {} Channel open error:{}".format(ip_address, reason)
+    def __init__(self, ip_address):
+        self.ip_address = ip_address
+        self.msg = "Host {} Timeout Error".format(ip_address)
         super().__init__(self.msg)
