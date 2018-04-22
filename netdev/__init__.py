89647f68ea2c73cef676a9ec398c194ed940e91e
Yakovlev Sergey <selfuryon@gmail.com>
2018-04-23 00:20:44 +0500

    Add ability to find out problems in commits

--- netdev/__init__.py
+++ netdev/__init__.py
@@ -1,7 +1,7 @@
 import netdev.vendors
 from netdev.dispatcher import create, platforms
-from netdev.exceptions import DisconnectError, TimeoutError
+from netdev.exceptions import DisconnectError, TimeoutError, CommitError
 from netdev.logger import logger
 from netdev.version import __author__, __author_email__, __url__, __version__
 
-__all__ = ('create', 'platforms', 'logger', 'DisconnectError', 'TimeoutError', 'vendors')
+__all__ = ('create', 'platforms', 'logger', 'DisconnectError', 'TimeoutError', 'CommitError', 'vendors')
--- netdev/exceptions.py
+++ netdev/exceptions.py
@@ -4,7 +4,7 @@ class DisconnectError(Exception):
         self.ip_address = ip_address
         self.code = code
         self.reason = reason
-        self.msg = "Host {} Disconnect Error:{}".format(ip_address, reason)
+        self.msg = "Host {} Disconnect Error: {}".format(ip_address, reason)
         super().__init__(self.msg)
 
 
@@ -14,4 +14,14 @@ class TimeoutError(Exception):
     def __init__(self, ip_address):
         self.ip_address = ip_address
         self.msg = "Host {} Timeout Error".format(ip_address)
+        super().__init__(self.msg)
+
+
+class CommitError(Exception):
+    """concurrent.futures._base.TimeoutError with ip address"""
+
+    def __init__(self, ip_address, reason):
+        self.ip_address = ip_address
+        self.reason = reason
+        self.msg = "Host {} Commit Error: {}".format(ip_address, reason)
         super().__init__(self.msg)
