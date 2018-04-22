b6b2f43f0d97008e4e897a331438e9e1e73072c9
Yakovlev Sergey <selfuryon@gmail.com>
2018-04-23 00:21:27 +0500

    Add examples with errors in commits

--- examples/cisco_iosxr.py
+++ examples/cisco_iosxr.py
@@ -16,20 +16,29 @@ async def task(param):
         # Testing sending simple command
         out = await iosxr.send_command("show ver")
         print(out)
+        # Testing sending simple command with long output
+        out = await iosxr.send_command("show run")
+        print(out)
         # Testing sending configuration set
         commands = ["interface loopback 0", "description TEST LOOPBACK"]
         out = await iosxr.send_config_set(commands, with_commit=True)
         print(out)
-        # Testing sending simple command with long output
-        out = await iosxr.send_command("show run")
+        # Testing failed commit
+        commands = ["interface GigabitEthernet 0/0/0/0", "service-policy input 1"]
+        out = await iosxr.send_config_set(commands, with_commit=False)
         print(out)
+        try:
+            commands = ["interface GigabitEthernet 0/0/0/0", "service-policy input 2"]
+            await iosxr.send_config_set(commands)
+        except netdev.CommitError:
+            print("Commit Error")
 
 
 async def run():
     config = yaml.load(open(config_path, 'r'))
     devices = yaml.load(open(config['device_list'], 'r'))
     tasks = [task(dev) for dev in devices if dev['device_type'] == 'cisco_ios_xr']
-    await asyncio.wait(tasks)
+    await asyncio.gather(*tasks)
 
 
 loop = asyncio.get_event_loop()
